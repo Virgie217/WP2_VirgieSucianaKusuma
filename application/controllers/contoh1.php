f77f0bc294136fd1fb16879a8354a6ae8b2b79a0
virgie217 <gienakusuma44@gmail.com>
2024-04-30 12:35:03 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Aura julia Azzahra
+              Saya tinggal di daerah warakas
+              Olahraga yang saya sukai adalah berenang";
+    }
+}
